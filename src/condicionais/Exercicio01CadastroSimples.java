a9d0970042d0cf9765ead73864341bcb1b0ddac2
Kimberly <kimberlydesouza0810@gmail.com>
2026-04-14 17:05:38 -0300

    adiciona exercício cadastro simples

--- src/condicionais/Exercicio01CadastroSimples.java
+++ src/condicionais/Exercicio01CadastroSimples.java
@@ -1,4 +1,18 @@
 package condicionais;
 
+import java.util.Scanner;
+
 public class Exercicio01CadastroSimples {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.printf("Olá %s! %nSua idade é: %d", nome, idade);
+
+    }
 }
